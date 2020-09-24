d43be2b059a7bf8a4ef1b67e34ba0212d237fd85
= <=>
2020-09-23 19:30:43 -0600

    some sql

---
+++ scratch.sql
@@ -0,0 +1,76 @@
+CREATE TEMP
+FUNCTION myMinHash
+(inputText STRING)
+  RETURNS ARRAY<INT64>
+  LANGUAGE js
+  OPTIONS
+(
+    library=["gs://jjames-bq-udfs/minhash_udf.js"]
+  )
+  AS
+"""
+//https://cloud.google.com/bigquery/docs/reference/standard-sql/user-defined-functions#including-javascript-libraries
+  return generateMinHash(inputText);
+""";
+
+CREATE OR REPLACE TABLE minhash_testing.dbpedia_hashed AS
+SELECT URI, rdf_schema_comment, myMinHash( rdf_schema_comment ) as minhash
+FROM `fh
+-bigquery.dbpedia.place`
+
+-- 
+
+CREATE OR REPLACE TABLE minhash_testing.dbpedia_bands AS
+SELECT URI, element, index,
+    CASE 
+    WHEN index < 5 THEN 'band1'
+    WHEN index < 10 THEN 'band2'
+    WHEN index < 15 THEN 'band3'
+    WHEN index < 20 THEN 'band4'
+    WHEN index < 25 THEN 'band5'
+    WHEN index < 30 THEN 'band6'
+    WHEN index < 35 THEN 'band7'
+    WHEN index < 40 THEN 'band8'
+    WHEN index < 45 THEN 'band9'
+    WHEN index < 50 THEN 'band10'
+    WHEN index < 55 THEN 'band11'
+    WHEN index < 60 THEN 'band12'
+    WHEN index < 65 THEN 'band13'
+    WHEN index < 70 THEN 'band14'
+    WHEN index < 75 THEN 'band15'
+    WHEN index < 80 THEN 'band16'
+    WHEN index < 85 THEN 'band17'
+    WHEN index < 90 THEN 'band18'
+    WHEN index < 95 THEN 'band19'
+    WHEN index < 100 THEN 'band20'
+    WHEN index < 105 THEN 'band21'
+    WHEN index < 110 THEN 'band22'
+    WHEN index < 115 THEN 'band23'
+    WHEN index < 120 THEN 'band24'
+    WHEN index < 125 THEN 'band25'
+    WHEN index < 130 THEN 'band26'
+ELSE 'other' END as band
+FROM `minhash_testing
+.dbpedia_hashed`, UNNEST
+(minhash) element
+WITH offset as index
+
+
+-- 
+
+WITH
+    base_table
+    AS
+    (
+        SELECT URI, band, ARRAY_TO_STRING(ARRAY_AGG(CAST(element as STRING)
+        ORDER BY index
+    ), '|') as band_key
+  FROM minhash_testing.dbpedia_bands
+  GROUP BY 1,2
+)
+
+SELECT band_key, band, array_agg(URI)
+FROM base_table
+GROUP BY 1,2
+
+-- Iterating over the bands and elements
